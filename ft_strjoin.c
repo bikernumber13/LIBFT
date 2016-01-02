839a02f56c9d126b1aa5028f6825c0560f51100b
Michael BOUHIER <mbouhier@e1r7p5.42.fr>
2016-01-02 11:50:18 +0100

    correction ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,35 +6,38 @@
 /*   By: mbouhier <mbouhier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 15:44:24 by mbouhier          #+#    #+#             */
-/*   Updated: 2016/01/02 11:08:02 by mbouhier         ###   ########.fr       */
+/*   Updated: 2016/01/02 11:45:21 by mbouhier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
+
 char	*ft_strjoin(const char *s1, const char *s2)
 {
-	unsigned int	len;
-	unsigned int	count1;
-	unsigned int	count2;
-	char			*buf;
+	int		len;
+	int		count1;
+	int		count2;
+	char	*buf;
 
 	count1 = 0;
-	if (s1 && s2)
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	if (!(buf = ft_strnew(len)))
+		return (NULL);
+	while (s1[count1])
 	{
-		len = ft_strlen(s1) + ft_strlen(s2);
-		if ((buf = (char *)malloc(sizeof(char *) * len + 1)) == NULL)
-			return (NULL);
-		while (count1 < len && s1[count1] != '\0')
-		{
-			buf[count1] = s1[count1];
-			count1++;
-		}
-		count2 = 0;
-		while (count1 < len && s2[count2] != '\0')
-			buf[count1++] = s2[count2++];
-		buf[count1] = '\0';
-		return (buf);
+		buf[count1] = s1[count1];
+		count1++;
 	}
-	return (0);
+	count2 = 0;
+	while (s2[count2])
+	{
+		buf[count1] = s2[count2];
+		count1++;
+		count2++;
+	}
+	buf[count1] = '\0';
+	return (buf);
 }
